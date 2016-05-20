aa184865b0123b08db63e8e25e86f8ac0b414dfe
Sandilya Jandhyala <jysandilya@gmail.com>
2016-05-20 20:24:34 +0530

    Wrote & fixed tests

--- tests/test_binary_string.hpp
+++ tests/test_binary_string.hpp
@@ -13,7 +13,7 @@ BOOST_AUTO_TEST_CASE(test_without_expiry)
     std::string contents = "some contents";
     auto bstr = binary_string(string_to_vec(contents));
     BOOST_CHECK(!bstr.has_expired());
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     BOOST_CHECK(!bstr.has_expired());
     BOOST_CHECK_EQUAL(contents, vec_to_string(bstr.bdata()));
 }
@@ -21,9 +21,9 @@ BOOST_AUTO_TEST_CASE(test_without_expiry)
 BOOST_AUTO_TEST_CASE(test_with_expiry)
 {
     std::string contents = "some contents";
-    auto bstr = binary_string(string_to_vec(contents), 2000);
+    auto bstr = binary_string(string_to_vec(contents), 200);
     BOOST_CHECK(!bstr.has_expired());
-    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
     BOOST_CHECK(bstr.has_expired());
     BOOST_CHECK_EQUAL(contents, vec_to_string(bstr.bdata()));
 }
